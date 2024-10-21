b20da667e41993f999df22d5a00294283f6bae0a
Suyash-Singh14 <suyashsingh1313@gmail.com>
2024-10-21 14:03:17 +0530

    parallax added on intro

--- static/js/main.js
+++ static/js/main.js
@@ -1,20 +1,30 @@
-let base = document.getElementsByClassName("base");
-// let box1 =document.getElementById("#box1");
-let box2 = document.getElementById("box2");
-// let box3 =document.getElementById("#box3");
-// let box4 =document.getElementById("#box4");
-let spacer = document.getElementsByClassName("spacer");
+let box1 =  document.querySelector(".intro .subgrid>.column:nth-child(1)");
+let box2 =  document.querySelector(".intro .subgrid>.column:nth-child(2)");
+let box3 =  document.querySelector(".intro .subgrid>.column:nth-child(3)");
+let box4 =  document.querySelector(".intro .subgrid>.column:nth-child(4)");
+let video =  document.querySelector(".intro .subgrid>.column:nth-child(5)");
+
 
 window.addEventListener('scroll', ()=>{
-    let value = window.scrollY;
-    base.style.marginBottom = value * 0.5 + "px";
+  box1.style.bottom = 0.5 * window.scrollY + "px";
+  box4.style.bottom = 0.5 * window.scrollY + "px";
+  box2.style.bottom = 0.2 * window.scrollY + "px";
+  box3.style.bottom = 1 * window.scrollY + "px";
+  video.style.bottom = 0.5 * window.scrollY + "px";
 })
 
-// Behind the Scenes
-let myTarget = document.querySelector('.target')
+let testbox1 =  document.querySelector(".Testimonials .subgrid>.column:nth-child(1)");
+let testbox2 =  document.querySelector(".Testimonials .subgrid>.column:nth-child(2)");
+let testbox3 =  document.querySelector(".Testimonials .subgrid>.column:nth-child(3)");
+let testbox4 =  document.querySelector(".Testimonials .subgrid>.column:nth-child(4)");
+let Testimonials = document.querySelector(".Testimonials");
 
-window.addEventListener('scroll', function() {
-  if(myTarget.offsetTop - window.scrollY <= 0){
-    myTarget.style.color = "red"
+window.addEventListener('scroll', ()=>{
+  if(Testimonials.offsetTop - window.scrollY <=0){
+    console.log("scroll");
+    testbox1.style.bottom = 0.5 * window.scrollY + "px";
+    testbox4.style.bottom = 0.5 * window.scrollY + "px";
+    testbox2.style.bottom = 0.2 * window.scrollY + "px";
+    testbox3.style.bottom = 1 * window.scrollY + "px";
   }
 })
